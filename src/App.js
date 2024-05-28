4735f98b60506b42dce7d5670fcecdb047ede769
AWalia-12 <amar.walia@icloud.com>
2024-05-28 17:19:47 -0500

    new

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
     <div className='full bg-blue w-full h-screen'>
       <h1 className='text-center text-'>Optotype </h1>
       <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
