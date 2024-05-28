0f8550c5561b9954bd4a399c158bcd956feddfb1
Benjamin_Horowitz <hhorowitz734@gmail.com>
2024-05-28 16:35:24 -0500

    did basic tailwind testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Counter from './counter';
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Optotype
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='full bg-blue w-full h-screen'>
+      <h1 className='text-center text-'>Optotype </h1>
+      <Counter />
     </div>
   );
 }
