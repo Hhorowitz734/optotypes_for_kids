be00c54b35dbe2f65b4e636dea2df2d282d250e5
Benjamin_Horowitz <hhorowitz734@gmail.com>
2024-06-08 14:14:19 -0500

    created usestate hook for score

--- src/pages/OptotypeQuestion.jsx
+++ src/pages/OptotypeQuestion.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ODisplay from '../components/ODisplay.jsx';
 
-const OptotypeQuestion = () => {
-    return (
-      
-      <div className="flex justify-center w-full">
 
-        <div className='mt-2'><ODisplay /></div>
+const OptotypeQuestion = () => {
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => setCount(count + 1);
+
+    
+
+    return (
+      <div>
+        <h1 className='m-4 text-2xl'>Counter: 0</h1>
+        <div className="flex justify-center w-full">
+
+          
+          <div className='mt-2'><ODisplay /></div>
+        </div>
       </div>
 
 
