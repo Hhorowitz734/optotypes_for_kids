7f6c33a824df69e2b78583bc3d65eabb52ba84a1
AWalia-12 <amar.walia@icloud.com>
2024-07-04 19:31:11 +0200

    made changes to ORight variable/functionality

--- src/components/ODisplay.jsx
+++ src/components/ODisplay.jsx
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
 
 class ODisplay extends Component {
-
     constructor(props) {
         super(props);
-
+        this.state = {
+            chartSizesMM: [20, 15, 10, 7.5, 5, 2.5, 2],
+            currentSizeIndex: 0,
+            isCorrect: true // You can manage this state from parent component or based on your logic
+        };
     }
 
-  
+    componentDidMount() {
+        this.updateImageSize();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.isCorrect !== this.state.isCorrect && this.state.isCorrect) {
+            this.updateImageSize();
+        }
+    }
+
+    updateImageSize() {
+        if (this.state.isCorrect && this.state.currentSizeIndex < this.state.chartSizesMM.length - 1) {
+            this.setState(prevState => ({
+                currentSizeIndex: prevState.currentSizeIndex + 1
+            }));
+        }
+    }
 
     render() {
 
-      const { optotype } = this.props;
+        const { optotype, chartSizesMM, currentSizeIndex } = this.props;
+        const sizeInMM = chartSizesMM[currentSizeIndex];
 
-      return (
-          <div>
-            <div className='flex items-center justify-center'>
-                <img src={optotype} className='border-2 border-black rounded-lg w-1/2' alt="Optotype" />
+        const style = {
+            width: `${sizeInMM}mm`,
+            height: `${sizeInMM}mm`
+        };
+
+
+        return (
+            <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+                <div className='border-2 border-black rounded-lg flex items-center justify-center' style={style}>
+                    <img src={optotype} alt="Optotype" className='object-contain' />
+                </div>
             </div>
-          </div>
-      );
-      
+        );
     }
 }
 
 export default ODisplay;
+
+
+
+
 
